488380452db86e758411182e901dc8e3735b4ff1
krtsato <ev60596@meiji.ac.jp>
2021-01-03 02:33:48 +0900

    feat: create dump template shell

---
+++ gin-gorm-logrus-basic/scripts/mysql/dump.sh
@@ -0,0 +1,29 @@
+#!/bin/sh -eu
+
+# example: ./scripts/mysql/dump.sh db-name table-name
+# table-name is optional. Empty string is a default value.
+table=
+suffix=_
+readonly DUMP_DIR=/var/dump
+
+if [ -z $1 ]; then
+    echo "Enter the DB name.\nYou can also fill in the table name if you want."
+    exit 1
+fi
+suffix="${suffix}$1"
+
+if [ ! -z ${2-} ]; then
+  suffix="${suffix}_$2"
+  table=$2
+fi
+
+mysqldump -u root -proot_pwd -h mysql -P 3306 --protocol=tcp \
+  --extended-insert \
+  --complete-insert \
+  --compress \
+	--default-character-set=binary \
+	--single-transaction \
+	--quick \
+	$1 $table > ${DUMP_DIR}/$(date "+%Y%m%d")${suffix}.dump
+
+echo "Dumped your database into the container ${DUMP_DIR}."
