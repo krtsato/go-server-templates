51a5ca42dd0c2af744e4843d538d4104c2e4e60a
krtsato <ev60596@meiji.ac.jp>
2021-01-25 04:20:13 +0900

    feat: wire generation

---
+++ gin-gorm-logrus-basic/cmd/gglb-gin-api/wire.go
@@ -0,0 +1,22 @@
+//+build wireinject
+
+package main
+
+import (
+	"github.com/google/wire"
+	"github.com/krtsato/go-rest-templates/gin-gorm-logrus-basic/internal/config"
+	"github.com/krtsato/go-rest-templates/gin-gorm-logrus-basic/internal/webapi"
+	"github.com/krtsato/go-rest-templates/gin-gorm-logrus-basic/internal/webapi/controller"
+)
+
+func InitializeGinApp(webCfg config.Web /*, hlxEnv hlx.AppEnv*/) (*webapi.GinApp, error) {
+	wire.Build(
+		// hlx.InitDB,
+		// gglbdb.NewAccountClientImpl,
+		// service.InjectNetworkImpl,
+		//handler.InjectNetwork,
+		controller.InjectSystem,
+		webapi.NewGinApp,
+	)
+	return nil, nil
+}
