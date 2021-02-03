f2ba66fc1d2caaad65347186389cd5d45b69f917
krtsato <ev60596@meiji.ac.jp>
2021-02-04 03:06:15 +0900

    feat: apierr telling internal server err to client

--- gin-gorm-logrus-basic/internal/webapi/apierr/api_err.go
+++ gin-gorm-logrus-basic/internal/webapi/apierr/api_err.go
@@ -1,1 +1,28 @@
 package apierr
+
+// APIError Web API で取り扱うエラー
+type APIErr interface {
+	ErrorCode() ErrCode
+	StatusCode() int
+	Error() string
+}
+
+// ErrCode APIErr 識別型
+type ErrCode int
+
+// ErrCode 種類
+const (
+	UnknownErrCode ErrCode = iota // 無効値
+	PublicErrCode
+	PrivateErrCode
+)
+
+var errCodeValueMap = map[ErrCode]string{
+	PublicErrCode:  "publicErrCode",
+	PrivateErrCode: "privateErrCode",
+}
+
+// String ErrCode 文字列を返却
+func (c ErrCode) String() string {
+	return errCodeValueMap[c]
+}
--- gin-gorm-logrus-basic/internal/webapi/apierr/server_err.go
+++ gin-gorm-logrus-basic/internal/webapi/apierr/server_err.go
@@ -1,1 +1,63 @@
 package apierr
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// InternalServer internal server error 500
+// NOTE: クライアントに返却する Internal Server Error は原則 500 番とする
+// アプリ内部で取り扱うエラーは internal/apperr で定義する
+type InternalServer struct {
+	ErrCode ErrCode
+	StsCode int
+	Cause   error
+}
+
+// ErrorCode ErrCode を返却
+func (i *InternalServer) ErrorCode() ErrCode {
+	if i == nil {
+		return UnknownErrCode
+	}
+	return i.ErrCode
+}
+
+// StatusCode StsCode を返却
+func (i *InternalServer) StatusCode() int {
+	if i == nil {
+		return 500
+	}
+	if i.StsCode == 0 {
+		return 500
+	}
+	return i.StsCode
+}
+
+// Error エラー文字列を返却
+func (i *InternalServer) Error() string {
+	if i == nil {
+		return ""
+	} else if i.Cause == nil {
+		return http.StatusText(i.StatusCode())
+	} else {
+		return i.Cause.Error()
+	}
+}
+
+// NewInternalServer InternalErr を err, msg から生成
+func NewInternalServer(errCode ErrCode, stsCode int, err error) *InternalServer {
+	if err == nil {
+		return &InternalServer{ErrCode: UnknownErrCode, StsCode: 500}
+	}
+	return &InternalServer{ErrCode: errCode, StsCode: stsCode, Cause: err}
+}
+
+// NewInternalServerE InternalServer を errCode, err から生成
+func NewInternalServerE(errCode ErrCode, err error) *InternalServer {
+	return NewInternalServer(errCode, 500, err)
+}
+
+// NewInternalServerF InternalServer を errCode, formatted err から生成
+func NewInternalServerF(errCode ErrCode, format string, a ...interface{}) *InternalServer {
+	return NewInternalServer(errCode, 500, fmt.Errorf(format, a...))
+}
