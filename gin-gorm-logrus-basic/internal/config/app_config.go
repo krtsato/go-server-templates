3db4d1def67c860eb01b75ed366a9ae5e9c97ecb
krtsato <ev60596@meiji.ac.jp>
2021-02-07 22:36:08 +0900

    fix: the way of merge by custom transformer

--- gin-gorm-logrus-basic/internal/config/app_config.go
+++ gin-gorm-logrus-basic/internal/config/app_config.go
@@ -3,6 +3,7 @@ package config
 import (
 	"fmt"
 	"io/ioutil"
+	"reflect"
 
 	"github.com/imdario/mergo"
 	"github.com/krtsato/go-rest-templates/gin-gorm-logrus-basic/internal/apperr"
@@ -18,7 +19,7 @@ const (
 // AppConfig 環境単位の設定値
 type AppConfig struct {
 	AppEnv string `yaml:"appEnv"`
-	// DB  GglbDB      `yaml:"db"`
+	// DB     DB            `yaml:"db"`
 	Log logger.Config `yaml:"log"`
 	Web Web           `yaml:"web"`
 }
@@ -35,8 +36,8 @@ func LoadConfig(e AppEnv) (*AppConfig, error) {
 		return &AppConfig{}, readErr
 	}
 
-	appConfigs := new(AppConfigs)
-	if err := yaml.Unmarshal(ymlConfigs, appConfigs); err != nil {
+	var appConfigs AppConfigs
+	if err := yaml.Unmarshal(ymlConfigs, &appConfigs); err != nil {
 		return &AppConfig{}, err
 	}
 
@@ -51,20 +52,32 @@ func LoadConfig(e AppEnv) (*AppConfig, error) {
 
 // targetConfig ゼロ値フィールドにのみ defaultConfig をマージ
 func (configs AppConfigs) merge(e AppEnv) (*AppConfig, error) {
-	var defaultConfig, targetConfig, emptyConfig *AppConfig
+	var defaultConfig, targetConfig, emptyConfig AppConfig
 	for _, config := range configs {
 		if config.AppEnv == e.String() {
-			targetConfig = config
+			targetConfig = *config
 		} else if config.AppEnv == "default" {
-			defaultConfig = config
+			defaultConfig = *config
 		}
 	}
 
 	if targetConfig == emptyConfig {
-		return &AppConfig{}, apperr.NewConfigErrF("unknown profile %s", e.String())
+		return &AppConfig{}, apperr.NewConfigErrF("unknown config profile %s", e.String())
 	}
-	if err := mergo.Merge(targetConfig, defaultConfig); err != nil {
+	if err := mergo.Merge(&targetConfig, defaultConfig, mergo.WithTransformers(boolTransformer{})); err != nil {
 		return &AppConfig{}, apperr.NewConfigErr(err, "failed to Merge by mergo")
 	}
-	return targetConfig, nil
+	return &targetConfig, nil
+}
+
+type boolTransformer struct{}
+
+// Transformer Mergo.merge() のカスタマイズ
+// Web.AuthCheck をゼロ値で上書きするため
+// dst 未定義のフィールドを src からマージするため
+func (b boolTransformer) Transformer(typ reflect.Type) func(dst, src reflect.Value) error {
+	if typ == reflect.TypeOf(Web{}) {
+		return func(dst, src reflect.Value) error { return nil }
+	}
+	return nil
 }
