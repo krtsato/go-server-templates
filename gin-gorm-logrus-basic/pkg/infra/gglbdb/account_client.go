93857711360cbb10e6996f356c5f7a2295e0e362
krtsato <ev60596@meiji.ac.jp>
2021-02-21 22:56:41 +0900

    feat: account client interface

--- gin-gorm-logrus-basic/pkg/infra/gglbdb/account_client.go
+++ gin-gorm-logrus-basic/pkg/infra/gglbdb/account_client.go
@@ -1,1 +1,42 @@
 package gglbdb
+
+import (
+	"context"
+	"github.com/krtsato/go-rest-templates/gin-gorm-logrus-basic/pkg/infra"
+)
+
+type AccountClient interface {
+	FindByAccountID(ctx context.Context, accountID int) (*Account, error)
+	FindByParam(ctx context.Context, param *FindAccountParam, pagination *Pagination) (Accounts, *Page, error)
+	UpdateByParam(ctx context.Context, accountID int, param *UpdateAccountParam) (*Account, error)
+	Save(ctx context.Context, account *Account) (*Account, error)
+}
+
+type accountClientImpl struct {
+	dbm *infra.DBManager
+}
+
+// NewAccountClientImpl DB manager を渡して client を生成
+func NewAccountClientImpl(dbm *infra.DBManager) AccountClient {
+	return &accountClientImpl{dbm: dbm}
+}
+
+// 初期 DI 以外のタイミングで DB manager を渡して client を生成
+func DefaultAccountClientImpl() AccountClient {
+	return &accountClientImpl{dbm: infra.GetDBManager()}
+}
+
+func (a *accountClientImpl) FindByAccountID(ctx context.Context, accountID int) (*Account, error) {
+	return &Account{}, nil
+}
+
+func (a *accountClientImpl) FindByParam(ctx context.Context, param *FindAccountParam, pagination *Pagination) (Accounts, *Page, error) {
+	return Accounts{}, nil, nil
+}
+func (a *accountClientImpl) UpdateByParam(ctx context.Context, accountID int, param *UpdateAccountParam) (*Account, error) {
+	return &Account{}, nil
+}
+
+func (a *accountClientImpl) Save(ctx context.Context, account *Account) (*Account, error) {
+	return &Account{}, nil
+}
---
+++ gin-gorm-logrus-basic/pkg/infra/gglbdb/param.go
@@ -0,0 +1,27 @@
+package gglbdb
+
+import "database/sql"
+
+// FindAccountParam 検索用パラメタ
+// オプショナルなパラメタのため使わない場合はゼロ値を設定
+// WHERE 句で絞り込み
+type FindAccountParam struct {
+	Name         string
+	ThxCount     string
+	ThxCountCond int8
+}
+
+// UpdateAccountParam 更新用パラメタ
+type UpdateAccountParam struct {
+	Name     string
+	Note     sql.NullString
+	ThxCount int
+}
+
+// NewWithUpdateParam 更新用パラメタから null skip する Account を生成
+func (a *Account) NewWithUpdateParam(param UpdateAccountParam) *Account {
+	if param.Note.Valid {
+		a.Note = param.Note
+	}
+	return a
+}
