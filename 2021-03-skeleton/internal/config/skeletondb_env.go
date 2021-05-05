b0210944344ee9ed2ca5d2eef6b65107e64d586e
krtsato <ev60596@meiji.ac.jp>
2021-05-06 04:26:54 +0900

    fix: var name

--- 2021-03-skeleton/internal/config/skeletondb_env.go
+++ 2021-03-skeleton/internal/config/skeletondb_env.go
@@ -5,5 +5,5 @@ import "github.com/krtsato/go-server-templates/2021-03-skeleton/pkg/infra"
 // DB database env
 type DB struct {
 	Env        infra.DBEnv      `yaml:"env"`
-	skeletonDB infra.Datasource `yaml:"skeletondb"`
+	SkeletonDB infra.Datasource `yaml:"skeletondb"`
 }
